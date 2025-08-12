9e4fe10eac3da14df4f1efc18215ce5746fb0a40
Praises003 <praiseabiodun15@gmail.com>
2025-08-12 09:38:08 -0700

    update to the alphavantage.ts

--- src/utils/alphavantage.ts
+++ src/utils/alphavantage.ts
@@ -7,7 +7,7 @@ export const getStockPrice = async (symbol: string) => {
   const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${API_KEY}`;
 
   const response = await axios.get(url);
-  const price = parseFloat(response.data["Global Quote"]["05. price"]);
+  const price = parseFloat(response.data?.["Global Quote"]?.["05. price"]);
 
   return price;
 };
